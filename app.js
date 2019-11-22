9c9dacd4cf4bffafe6b531053be2b7cb438f4faf
Anton <anton.kornilkov@domru.ru>
2019-11-22 17:33:29 +0500

    Ready for use

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var session = require('express-session');
+var cors = require('cors');
+var MongoDB = require('./models/user').Mongo;
+
+MongoDB.connect('mongodb://localhost:27017/database', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('MongoDB is connected')
+}).catch(error => {
+    console.log('MongoDB is NOT connected.' + error)
+})
+
+require('./controllers/passport')(passport);
 
 var app = express();
-
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger('short'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'));
+// Express session
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/users', require('./routes/users'));
 
