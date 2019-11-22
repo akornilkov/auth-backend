845ef7a137f4758a572113a82ac946603bdf50f4
Anton <anton.kornilkov@domru.ru>
2019-11-22 15:40:08 +0500

    Development started

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', require('./routes/users'));
+
+module.exports = app;
